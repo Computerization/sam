9e9927762a301b79f90d26e007479dbbada7daf1
David Zhong <davidzyc2000@hotmail.com>
2017-08-14 07:56:00 +0800

    Welcome page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,12 @@
 
                 <div class="panel-body">
                     You are logged in!
+                    <hr>
+                    <h2>Vote</h2>
                     <h4><a href="{{ url('/vote') }}">Click Here to Proceed to Vote Page.</a></h4>
+                    <hr>
+                    <h2>Reserve a Classroom</h2>
+                    <h4><a href="{{ url('/order/room') }}">Click Here to Proceed to Reservation Page.</a></h4>
                 </div>
             </div>
         </div>
