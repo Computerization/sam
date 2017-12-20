c3e35c343e9b50111a7be1a4482067a4ab4b4fac
David Zhong <davidzyc2000@hotmail.com>
2017-12-20 17:51:04 +0800

    d-4 to d-1

--- resources/views/auction/auction.blade.php
+++ resources/views/auction/auction.blade.php
@@ -35,7 +35,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">最高价格</h4>
-                        <h1 class="display-4">￥<span id="cur-bid"></span></h1>
+                        <h1 class="display-1">￥<span id="cur-bid"></span></h1>
                         {{--  <p><span id="cur-bid-uname"></span> 于 <span id="cur-bid-time"></span></p>  --}}
                         <p><span id="cur-bid-time"></span></p>
                     </div>
