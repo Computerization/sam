6231fd516ee56c8a31e54ecdd0fd6ee261776417
David Zhong <davidzyc2000@hotmail.com>
2017-12-21 16:45:42 +0800

    disable https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        $this->app['request']->server->set('HTTPS', $this->app->environment() != 'local');
+        // $this->app['request']->server->set('HTTPS', $this->app->environment() != 'local');
     }
 
     /**
