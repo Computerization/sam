6a9f99de32396bccc13eaaf6f89ed89099775715
David Zhong <davidzyc2000@hotmail.com>
2017-12-22 17:07:56 +0800

    Navbar UI Update

--- resources/views/layouts/app_bs4.blade.php
+++ resources/views/layouts/app_bs4.blade.php
@@ -18,11 +18,21 @@
 <body>
 
     <!-- Image and text -->
-    <nav class="navbar navbar-dark bg-dark mb-4">
+    <nav class="navbar  navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="https://sam.swfla.org/images/logo.svg" width="30" height="30" class="d-inline-block align-top" alt=""> SAM
+                <img src="https://sam.swfla.org/images/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+                {{--  <span class="ml-2">SAM</span>  --}}
             </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                {{--  <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>  --}}
+                <a class="nav-item nav-link" href="{{ url('org') }}">{{ trans('org.org') }}</a>
+                </div>
+            </div>
         </div>
     </nav>
 
