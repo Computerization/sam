be3ed90e3e1155cf32e5c805f2152f2499d7c732
David Zhong <davidzyc2000@hotmail.com>
2018-01-12 22:10:49 +0800

    Qcount update

--- resources/views/votes/question/radio.blade.php
+++ resources/views/votes/question/radio.blade.php
@@ -1,5 +1,4 @@
 @php
-  $qnum = 0;
   $question_json = json_decode($question->question_content, true);
 @endphp
 
@@ -9,11 +8,10 @@
   <div class="radio">
     <label>
       @php
-      $qnum ++;
       $qcount = DB::table("answers")->where("question_id", $question->id)->where("answer_content", $choice["value"])->count()
       @endphp
-      @if(!($qnum == 1 && $qcount >= 10)) <input type="radio" name="answer[{{ $question->id }}][answer_content]" value='{{ $choice["value"] }}' @if($question->answers->where('user_id',Auth::id())->count()>0) @if($question->answers->where('user_id',Auth::id())->first()->answer_content == $choice["value"]) checked @endif @endif> @else （已选满） @endif
-      {{ $choice["content"] }} - {{ $qcount }}人已选
+      @if((!isset($choice["qcount"])) || (isset($choice["qcount"]) && $qcount <= $choice["qcount"])) <input type="radio" name="answer[{{ $question->id }}][answer_content]" value='{{ $choice["value"] }}' @if($question->answers->where('user_id',Auth::id())->count()>0) @if($question->answers->where('user_id',Auth::id())->first()->answer_content == $choice["value"]) checked @endif @endif> @else （已选满） @endif
+      {{ $choice["content"] }} - {{ $qcount }}人已选@if(isset($choice["qcount"]))，共{{ $choice["qcount"]}}名额@endif
     </label>
   </div>
   @endif
