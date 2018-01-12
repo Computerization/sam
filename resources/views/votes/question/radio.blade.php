6cd300272d261f53f2410ae33013b310a7f26a96
David Zhong <davidzyc2000@hotmail.com>
2018-01-12 21:33:51 +0800

    Qcount Fix

--- resources/views/votes/question/radio.blade.php
+++ resources/views/votes/question/radio.blade.php
@@ -10,7 +10,7 @@
     <label>
       @php
       $qnum ++;
-      $qcount = DB::table("answers")->where("answer_content", $choice["value"])->count()
+      $qcount = DB::table("answers")->where("question_id", $question->id)->where("answer_content", $choice["value"])->count()
       @endphp
       @if(!($qnum == 1 && $qcount >= 10)) <input type="radio" name="answer[{{ $question->id }}][answer_content]" value='{{ $choice["value"] }}' @if($question->answers->where('user_id',Auth::id())->count()>0) @if($question->answers->where('user_id',Auth::id())->first()->answer_content == $choice["value"]) checked @endif @endif> @else （已选满） @endif
       {{ $choice["content"] }} - {{ $qcount }}人已选
