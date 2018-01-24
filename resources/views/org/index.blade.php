0819c6cdb82c984022d694b8f3cb49e9cc1d7421
David Zhong <davidzyc2000@hotmail.com>
2018-01-24 10:19:05 +0800

    control panel and org UI Update

--- resources/views/org/index.blade.php
+++ resources/views/org/index.blade.php
@@ -16,11 +16,11 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12 col-xs-12">
-          <div class="jumbotron">
+          <!-- <div class="jumbotron">
             <h1>2017 Why Charity 在线拍卖通道</h1>
             <p>请先点击右上角按钮注册，然后点击下面链接进入拍卖主界面。</p>
             <p><a class="btn btn-primary btn-lg" href="{{ url('auction') }}" role="button">点击进入</a></p>
-          </div>
+          </div> -->
 
           <div class="row">
             <div class="col-md-2">
@@ -40,7 +40,7 @@
           </div>
 
           <hr>
-          <div class="card-columns">
+          <div class="card-columns" style="column-count: 4;">
             @foreach($orgs as $org)
               @include('org.single_org', ['org' => $org, 'wide' => false])
             @endforeach
--- resources/views/org/single_org.blade.php
+++ resources/views/org/single_org.blade.php
@@ -1,30 +1,22 @@
 {{-- <div class="@if($wide) col-md-6 @else col-md-4 @endif col-sm-6 col-xs-12"> --}}
 
-  <div class="card">
-    <div class="card-body">
-      <div class="media">
-        <div class="media-left">
-          @if($org->file_id)
-          <img src="/image/{{ $org->file_id }}" width="96" alt="...">
-          @endif
-        </div>
-        <div class="media-body">
-          <h4 class="media-heading">
-            <a href="{{ url('org', $org->id) }}">{{ $org->organization_name }}</a>
-            @foreach($org->tags->all() as $tag)
-            <small><span class="label label-default">{{ $tag->tag_content }}</span></small>
-            @endforeach
-          </h4>
 
-          <p class="hideoverflow"><small>{{ $org->organization_intro }}</small></p>
+  <div class="card  bg-dark text-white" style="min-height: 250px;">
+    @if($org->file_id)
+    <img class="card-img-top"  style="opacity: 0.5;" src="/image/{{ $org->file_id }}" alt="Card image cap">
+    @endif
+    <div class="card-img-overlay">
+    <h5 class="card-title"><a class="a-dark" href="{{ url('org', $org->id) }}">{{ $org->organization_name }}</a></h5>
+    <p class="card-texgt"><i class="fa fa-user-o" aria-hidden="true"></i> {{ $org->user->name }}</p>
+    <p class="card-text"><small class="hideoverflow">{{ $org->organization_intro }}</small></p>
+
           @if(Auth::check())
           @if(Auth::id() == $org->user_id)
-                <a class="btn btn-sm btn-link" href="{{ URL::action('OrganizationController@edit', ['id' => $org->id]) }}">{{ trans('org.edit') }}</a>
-                <a class="btn btn-sm btn-link" href="{{ URL::action('OrganizationController@show_resumes', ['id' => $org->id]) }}">{{ trans('org.view_resumes') }}</a>
+                <a class="btn btn-sm btn-default" href="{{ URL::action('OrganizationController@edit', ['id' => $org->id]) }}">{{ trans('org.edit') }}</a>
+                <a class="btn btn-sm btn-default" href="{{ URL::action('OrganizationController@show_resumes', ['id' => $org->id]) }}">{{ trans('org.view_resumes') }}</a>
           @endif
           @endif
-        </div>
-      </div>
+
     </div>
   </div>
 
