cacdb886503143d4327d6be979e56f417be6fce8
Ye Shu <shuye02@outlook.com>
2018-09-05 04:18:58 +0800

    Setting up Eloquent Model for orderroom

--- app/Http/Controllers/RoomOrderController.php
+++ app/Http/Controllers/RoomOrderController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\orderroom;
+use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
--- app/orderroom.php
+++ app/orderroom.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class orderroom extends Model
 {
-    //
+    
+    protected $fillable = ['room_id', 'day', 'time', 'user_id', 'organization_id'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function organization(){
+        return $this->belongsTo('App\Organization');
+    }
 }
