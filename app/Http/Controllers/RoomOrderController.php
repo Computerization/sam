85362f75b29844e3bb8cab5b48e63e6d2964c0ca
Ye Shu <shuye02@outlook.com>
2018-09-05 04:20:35 +0800

    RoomOrderController@index

--- app/Http/Controllers/RoomOrderController.php
+++ app/Http/Controllers/RoomOrderController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\orderroom;
 use App\User;
@@ -12,9 +14,10 @@ class RoomOrderController extends Controller
 {
     //
     public function index(){
-      $order_arr = Storage::get('room_order.json');
-      $order = json_decode($order_arr, true);
-      return view('orderroom.index', ['order' => $order]);
+        // Select only entries updated this week
+        $order = orderroom::all()->where('organization_id', '!==', NULL)->where('updated_at', '>=', Carbon::now()->startOfWeek())->where('updated_at', '<=', Carbon::now()->endOfWeek());
+
+        return view('orderroom.show', ['order' => $order]);
     }
 
     public function process(Request $request){
