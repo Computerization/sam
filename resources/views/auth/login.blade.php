453f5488bc521c2881ddf5394750924ad91151b4
shuye02 <shuye02@outlook.com>
2018-11-15 15:55:43 +0800

    Fix csrf_token in login.blade.php Meanwhile make codes more organized

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,111 +1,63 @@
 @extends('layouts.semantic')
-
-    <head>
-        <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> -->
-        <link rel="stylesheet" type="text/css" href="semantic/semantic.min.css">      
-    </head>
 @section('content')
-    
-  
-<br>
+    <br />
     <div class="ui  grid stackable container">
-            
         <div class="three column row">
-    
-            <div class="ui  column"> 
-            </div>
-        
+            <div class="ui column"></div>
+            <div class="ui column">
+                <div class="ui segment">
+                    @if(session("reset") == 1)
+                        <div class="ui green icon message">
+                            <i class="check icon"></i>
+                            账号激活成功！
+                        </div>
+                    @endif
+                    <div class="ui center aligned container">
+                        <h1 class="ui header">{{ trans('login.login') }}</h1>
+                    </div>
 
-            <div class="ui column"><div class="ui segment">
-@if(session("reset") == 1)            
-                    <div class="ui green icon message">
-                        <i class="check icon"></i>
-                        账号激活成功！
-                    </div>   
-@endif
-        <div class="ui center aligned container">
-                <h1 class="ui header">{{ trans('login.login') }}</h1>
-        </div>      
-        <div class="ui divider"></div>
-                
-                <!-- <form role="form" method="POST" action="{{ route('login') }}" >  -->
-                    
-                        {{ csrf_field() }}
-                   
-                        
-                            
-                            <!-- <label for="email" class=" control-label">{{ trans('login.email') }}</label> -->
-                
+                    <div class="ui divider"></div>
 
                     <form role="form" method="POST" action="{{ route('login') }}">
-                            <div class="ui left icon fluid  input">
-                                <i class="envelope outline icon"></i>
-                                <input id="email" type="email" class="form-control" placeholder="邮箱" name="email" value="{{ old('email') }}" required autofocus>
-                            </div>
-                        
-                        
-                        
-@if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                        
-@endif
-                    
-                        
-                        <br>
-                        
-                            <!-- <label for="password" class=" control-label">{{ trans('login.password') }}</label> -->
-                    
-
-                       
-                            <div class="ui left icon fluid input">
-                                <i class="key icon"></i>
-                                <input id="password" type="password" class="form-control" placeholder="密码" name="password" required>
-                            </div>
-                       
-
-                        
-@if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                    
-@endif                        
-                        
-                        
-                        <br>
-                                
-                        <div class="ui checkbox">
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> 
-                            <label> {{ trans('login.remember') }} </label>
+                        {{ csrf_field() }}
+                        <div class="ui left icon fluid  input">
+                            <i class="envelope outline icon"></i>
+                            <input id="email" type="email" class="form-control" placeholder="邮箱" name="email" value="{{ old('email') }}" required autofocus>
                         </div>
-                                
-                            
-                        <br><br>
-
-                        <div class="ui center aligned container">
-                                
-                                <button type="submit" class="ui teal button">
-                                    <i class="sign in alternate icon"></i>
-                                    {{ trans('login.login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ trans('login.forget') }}
-                                </a>
-                        </div>
-                    </form>  
-            </div></div>
-
-            <div class="ui  column"> 
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                    <br />
+                    <div class="ui left icon fluid input">
+                        <i class="key icon"></i>
+                        <input id="password" type="password" class="form-control" placeholder="密码" name="password" required>
+                    </div>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                    <br />
+                    <div class="ui checkbox">
+                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label> {{ trans('login.remember') }} </label>
+                    </div>
+                    <br /><br />
+                    <div class="ui center aligned container">
+                        <button type="submit" class="ui teal button">
+                            <i class="sign in alternate icon"></i>
+                            {{ trans('login.login') }}
+                        </button>
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ trans('login.forget') }}
+                        </a>
+                    </div>
+                    </form>
             </div>
         </div>
-                <!-- </form>     -->
+        <div class="ui column"></div>
+        </div>
     </div>
-
-     
-
-
-
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,107 +2,81 @@
 
 @section('content')
     <div class="ui raised padded text container segment">
-                <div class="ui center aligned container">
-                    <h1 class="ui header">{{ trans('login.register') }}</h1>
-                </div>
-
-                <div class="ui divider"></div>
-
-               
-
-                    
-                <form  role="form" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
-                    <div class="ui stackable grid">
-                        <br>
-                        <div class="row">
-                            <div class="column">
-                                <div class="ui tiny fluid message">
-                                    填写用户名时，请输入您的真实姓名，以方便工作人员联系您。
-                                </div>
-                            </div>
-                        </div>
-                        <br>
-
-                        
-                            <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <div class="six wide column"><lable for="name">姓名</lable></div>
-
-                                <div class="ten wide column ">
-                                    <div class="ui fluid input">
-                                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-                                    </div>
-                                
-
-@if ($errors->has('name'))
-                                        <br>   
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('name') }}</strong>
-                                        </span>
-@endif
-                                </div>
-
-                            </div>
-                        
-
-                        <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <div class="six wide column"><lable for="email">{{ trans('login.email') }}</lable></div>
-
-                            <div class="ten wide column ">
-                                <div class="ui fluid input">
-                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-                                </div>
-                           
-
-    @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <i class="red warning sign icon"></i>
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-    @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <div class="six wide column"><lable for="password">{{ trans('login.password') }}</label></div>
-
-                            <div class="ten wide column ">
-                                <div class="ui fluid input">
-                                    <input id="password" type="password" class="form-control" name="password" required>
-                                </div>
-
-@if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <i class="red warning sign icon"></i>
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-@endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="six wide column"><lable for="password_confirm">{{ trans('login.confirm_password') }}</label></div>
-
-                            <div class="ten wide column ">
-                                <div class="ui fluid input">
-                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row form-group">
-                            <div class="center aligned column">
-                                <button type="submit" class="ui teal large button">
-                                    <i class="user plus icon"></i>
-                                    {{ trans('login.register') }}
-                                </button>
-                            </div>
+        <div class="ui center aligned container">
+            <h1 class="ui header">{{ trans('login.register') }}</h1>
+        </div>
+        <div class="ui divider"></div>
+            <form  role="form" method="POST" action="{{ route('register') }}">
+                {{ csrf_field() }}
+                <div class="ui stackable grid">
+                <br>
+                <div class="row">
+                    <div class="column">
+                        <div class="ui tiny fluid message">
+                            填写用户名时，请输入您的真实姓名，以方便工作人员联系您。
                         </div>
                     </div>
-                </form>
-                
-            
-        
+                </div>
+                <br />
+                <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <div class="six wide column"><lable for="name">姓名</lable></div>
+                    <div class="ten wide column ">
+                        <div class="ui fluid input">
+                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                        </div>
+                        @if ($errors->has('name'))
+                            <br>
+                            <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
+                    <div class="six wide column"><lable for="email">{{ trans('login.email') }}</lable></div>
+                    <div class="ten wide column ">
+                        <div class="ui fluid input">
+                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                        </div>
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <i class="red warning sign icon"></i>
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <div class="six wide column"><lable for="password">{{ trans('login.password') }}</label></div>
+                    <div class="ten wide column ">
+                        <div class="ui fluid input">
+                            <input id="password" type="password" class="form-control" name="password" required>
+                        </div>
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <i class="red warning sign icon"></i>
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="six wide column"><lable for="password_confirm">{{ trans('login.confirm_password') }}</label></div>
+                    <div class="ten wide column ">
+                        <div class="ui fluid input">
+                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                        </div>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="center aligned column">
+                        <button type="submit" class="ui teal large button">
+                            <i class="user plus icon"></i>
+                            {{ trans('login.register') }}
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
-
 @endsection
